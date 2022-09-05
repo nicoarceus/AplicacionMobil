6d218da2bdd529f6f6b8464aad7f1beb0335bc20
Nicolas Zamorano <na.zamovasquez@gmail.com>
2022-09-05 13:21:30 -0300

    se define home inservible

--- trabajo_sumativo/capacitor.config.ts
+++ trabajo_sumativo/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tablitas',
+  appName: 'trabajo_sumativo',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- trabajo_sumativo/src/app/app-routing.module.ts
+++ trabajo_sumativo/src/app/app-routing.module.ts
@@ -3,8 +3,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tablinks',
     loadChildren: () => import('./tablinks/tablinks.module').then( m => m.TablinksPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
   },
 ];
 
